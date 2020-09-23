0b9a77e66e8bc5f17bb785380e1e7f248b42de64
Kyle Madsen <kyle.mdsn@gmail.com>
2020-09-22 17:15:07 -0700

    working-ish

--- validator/src/releases.js
+++ validator/src/releases.js
@@ -11,6 +11,46 @@ function releaseInfo(relativePath, release) {
     return ('' + info).replace(/(\n)\s+/g, '$1');
 }
 
+function downloadReleaseService(release, service) {
+    const path = `mapbox/mapbox/com/mapbox/mapboxsdk/${service}`
+    const query = `${release}/${service}-${release}.jar`
+    const url = `https://dl.bintray.com/${path}/${query}`
+    const util = require('util')
+    console.log(url)
+
+    async function write(response) {
+        const streamPipeline = util.promisify(require('stream').pipeline)
+        const outputDirectory = "releases"
+        if (!fs.existsSync(outputDirectory)) {
+            fs.mkdirSync(outputDirectory);
+        }
+        const releaseDirectory = `${outputDirectory}/${release}`
+        if (!fs.existsSync(releaseDirectory)) {
+            fs.mkdirSync(releaseDirectory);
+        }
+        const relativePath = `./${releaseDirectory}/${service}.jar`
+        if (!fs.existsSync(relativePath)) {
+            console.log(`Downloading ${relativePath}`)
+            await streamPipeline(response.body, fs.createWriteStream(relativePath))
+        } else {
+            console.log(`Download already exists ${relativePath}`)
+        }
+
+        console.log(releaseInfo(relativePath, release))
+    };
+
+    async function download () {
+        const response = await fetch(url)
+            .then( response => {
+                if (!response.ok) throw new Error(`Release was not found ${response.statusText}`)
+                write(response);
+             })
+            .catch( error => console.error('Unable to download release:', error) );
+    };
+
+    download()
+}
+
 module.exports = {
     listReleases: function() {
         const versionUrl = "https://api.bintray.com/search/packages/maven?g=com.mapbox.mapboxsdk&a=mapbox-sdk-services"
@@ -21,40 +61,7 @@ module.exports = {
             .catch( error => console.error('Unable to list release versions:', error) );
     },
     downloadRelease: function(release) {
-        const path = "mapbox/mapbox/com/mapbox/mapboxsdk/mapbox-sdk-services"
-        const query = `${release}/mapbox-sdk-services-${release}.jar`
-        const url = `https://dl.bintray.com/${path}/${query}`
-        const util = require('util')
-        console.log(url)
-        
-        async function write(response) {
-            const streamPipeline = util.promisify(require('stream').pipeline)
-            const outputDirectory = "releases"
-            if (!fs.existsSync(outputDirectory)) {
-                fs.mkdirSync(outputDirectory);
-            }
-            const releaseDirectory = `${outputDirectory}/${release}`
-            const relativePath = `./${releaseDirectory}/mapbox-sdk-services.jar`
-            if (!fs.existsSync(releaseDirectory)) { 
-                fs.mkdirSync(releaseDirectory);
-                console.log(`Downloading ${release}`)
-                await streamPipeline(response.body, fs.createWriteStream(relativePath))
-            } else {
-                console.log(`The jar already exists ${release}`)
-            }
-            
-            console.log(releaseInfo(relativePath, release))
-        };
-
-        async function download () {
-            const response = await fetch(url)
-                .then( response => {
-                    if (!response.ok) throw new Error(`Release was not found ${response.statusText}`)
-                    write(response);
-                 })
-                .catch( error => console.error('Unable to download release:', error) );
-        };
-
-        download()
+        downloadReleaseService(release, "mapbox-sdk-services")
+        downloadReleaseService(release, "mapbox-sdk-geojson")
     },
 };
--- validator/src/runner.js
+++ validator/src/runner.js
@@ -1,29 +1,25 @@
-
-const fs = require('fs')
-const java = require('java');
-const jarfile = require("jarfile")
-const api = require('./releases')
-
 module.exports = {
     validateJson: function(release, file) {
-        api.downloadRelease(release)
+        const fs = require('fs')
+        const java = require('java');
 
         // const javaLangSystem = java.import('java.lang.System');
-
         // javaLangSystem.out.printlnSync('Hello World');
 
-        // Get the Main-Class entry from foo.jar.
-        const outputDirectory = "releases"
-        const releaseDirectory = `${outputDirectory}/${release}`
-        const relativePath = `./${releaseDirectory}/mapbox-sdk-services.jar`
+        const releaseDirectory = `releases/${release}`
 
-        var java = require("java");
-        java.classpath.push(relativePath);
+        java.classpath.push(`./${releaseDirectory}/mapbox-sdk-services.jar`);
+        java.classpath.push(`./${releaseDirectory}/mapbox-sdk-geojson.jar`);
+
+        // TODO :thinking:
         java.classpath.push("lib/gson-2.8.6.jar");
 
         const data = fs.readFileSync(file, 'utf8')
-        const className = "com.mapbox.api.directions.v5.models.DirectionsResponse";
-        const result = java.callStaticMethodSync(className, "fromJson", data)
-        console.log(result)
+
+        const directionStatic = java.import("com.mapbox.api.directions.v5.models.DirectionsResponse");
+        const result = directionStatic.fromJsonSync(data)
+        console.log(result.toJsonSync())
+        
+        console.log("Completed without a crash?")
     }
 }
